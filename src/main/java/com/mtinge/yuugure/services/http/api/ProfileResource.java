9dd2153cf383e6514a40399a143770ef050d9392
MidnightTinge <79157320+MidnightTinge@users.noreply.github.com>
2021-06-26 11:35:06 -0500

    Fix ProfileResource not using MethodValidator

--- src/main/java/com/mtinge/yuugure/services/http/api/ProfileResource.java
+++ src/main/java/com/mtinge/yuugure/services/http/api/ProfileResource.java
@@ -8,10 +8,10 @@ import com.mtinge.yuugure.data.http.SafeAccount;
 import com.mtinge.yuugure.data.postgres.DBAccount;
 import com.mtinge.yuugure.services.database.UploadFetchParams;
 import com.mtinge.yuugure.services.http.Responder;
+import com.mtinge.yuugure.services.http.util.MethodValidator;
 import io.undertow.Handlers;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.server.handlers.PathTemplateHandler;
-import io.undertow.util.Headers;
 import io.undertow.util.Methods;
 import io.undertow.util.StatusCodes;
 import org.slf4j.Logger;
@@ -50,38 +50,36 @@ public class ProfileResource extends APIResource<DBAccount> {
 
     if (resource.state == FetchState.OK) {
       if (action.equalsIgnoreCase("uploads")) {
-        if (exchange.getRequestMethod().equals(Methods.GET)) {
-          res.json(Response.good(App.database().getRenderableUploadsForAccount(resource.resource.id, new UploadFetchParams(false, authed != null && authed.id == resource.resource.id))));
-        } else if (exchange.getRequestMethod().equals(Methods.DELETE)) {
-          if (authed.id != resource.resource.id) {
-            res.json(Response.fromCode(StatusCodes.FORBIDDEN));
-          } else {
-            var token = extractConfirmationToken(exchange);
-            if (token != null) {
-              if (App.redis().confirmToken(token, authed, true)) {
-                try {
-                  var updated = App.database().jdbi().inTransaction(handle ->
-                    handle.createUpdate("UPDATE upload SET state = (state | :delete_state) WHERE owner = :owner AND (state & :delete_state) = 0")
-                      .bind("owner", authed.id)
-                      .bind("delete_state", States.Upload.DELETED)
-                      .execute()
-                  );
-                  res.json(Response.good().addMessage("Deleted " + updated + " uploads."));
-                } catch (Exception e) {
-                  logger.error("Failed to delete uploads for account {}.", authed.id, e);
-                  res.json(Response.fromCode(StatusCodes.INTERNAL_SERVER_ERROR));
+        if (MethodValidator.handleMethodValidation(exchange, Methods.GET, Methods.DELETE)) {
+          if (exchange.getRequestMethod().equals(Methods.GET)) {
+            res.json(Response.good(App.database().getRenderableUploadsForAccount(resource.resource.id, new UploadFetchParams(false, authed != null && authed.id == resource.resource.id))));
+          } else if (exchange.getRequestMethod().equals(Methods.DELETE)) {
+            if (authed.id != resource.resource.id) {
+              res.json(Response.fromCode(StatusCodes.FORBIDDEN));
+            } else {
+              var token = extractConfirmationToken(exchange);
+              if (token != null) {
+                if (App.redis().confirmToken(token, authed, true)) {
+                  try {
+                    var updated = App.database().jdbi().inTransaction(handle ->
+                      handle.createUpdate("UPDATE upload SET state = (state | :delete_state) WHERE owner = :owner AND (state & :delete_state) = 0")
+                        .bind("owner", authed.id)
+                        .bind("delete_state", States.Upload.DELETED)
+                        .execute()
+                    );
+                    res.json(Response.good().addMessage("Deleted " + updated + " uploads."));
+                  } catch (Exception e) {
+                    logger.error("Failed to delete uploads for account {}.", authed.id, e);
+                    res.json(Response.fromCode(StatusCodes.INTERNAL_SERVER_ERROR));
+                  }
+                } else {
+                  res.json(Response.fromCode(StatusCodes.UNAUTHORIZED).addMessage("Invalid confirmation token."));
                 }
               } else {
-                res.json(Response.fromCode(StatusCodes.UNAUTHORIZED).addMessage("Invalid confirmation token."));
+                res.json(Response.fromCode(StatusCodes.UNAUTHORIZED).addMessage("Missing confirmation token"));
               }
-            } else {
-              res.json(Response.fromCode(StatusCodes.UNAUTHORIZED).addMessage("Missing confirmation token"));
             }
           }
-        } else {
-          res
-            .header(Headers.ALLOW, "GET,DELETE")
-            .json(Response.fromCode(StatusCodes.METHOD_NOT_ALLOWED));
         }
       }
     } else {
