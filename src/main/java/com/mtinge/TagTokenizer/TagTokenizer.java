7d3bbe7d00e48d052e1e654f7c35b9809494878a
MidnightTinge <79157320+MidnightTinge@users.noreply.github.com>
2021-06-27 10:23:22 -0500

    Add TagTokenizer explainer

--- src/main/java/com/mtinge/TagTokenizer/TagTokenizer.java
+++ src/main/java/com/mtinge/TagTokenizer/TagTokenizer.java
@@ -2,6 +2,7 @@ package com.mtinge.TagTokenizer;
 
 import com.mtinge.TagTokenizer.lexer.LexToken;
 import com.mtinge.TagTokenizer.lexer.Lexer;
+import com.mtinge.TagTokenizer.tokenizer.TermModifier;
 import com.mtinge.TagTokenizer.tokenizer.Tokenizer;
 import com.mtinge.TagTokenizer.tokenizer.TokenizerToken;
 
@@ -18,5 +19,35 @@ public class TagTokenizer {
 
   public static LinkedList<TokenizerToken> parse(LinkedList<LexToken> tokens) throws SyntaxError {
     return Tokenizer.tokenize(tokens);
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  private static void _explain(LinkedList<TokenizerToken> tokens, int indent, StringBuilder sb) {
+    for (var token : tokens) {
+      sb.append(indent(indent)).append(token.type);
+      if (token.modifier != TermModifier.AND) {
+        sb.append(" {modifier=").append(token.modifier.toString().toLowerCase()).append("}");
+      }
+      if (token.type.equals(TokenizerToken.Type.TERM)) {
+        sb.append(": ").append(token.value).append("\n");
+      } else {
+        sb.append("\n");
+        // It is illegal to have an empty group and .children will never be null when the type is
+        // group.
+        _explain(token.children, indent + 1, sb);
+      }
+    }
+  }
+
+  public static String explain(LinkedList<TokenizerToken> tokens) {
+    var sb = new StringBuilder();
+
+    _explain(tokens, 0, sb);
+
+    return sb.toString();
+  }
+
+  private static String indent(int amount) {
+    return "\t".repeat(Math.max(0, amount));
   }
 }
